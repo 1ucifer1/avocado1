ca349e3d250a290d7f39098fbf361f1835c2191a
1ucifer1 <omarmshawky23@gmail.com>
2022-04-18 22:27:22 +0200

    legislation and caselist updated

---
+++ caselist.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            case_number:123,
+            client_name:"ahmed mohamed",
+            case_name:"ssssssssssssssssssssssssssss",
+            case_status:"ssssssssssssssssssssssssssssssssssssssssssssssssssss"
+        }
+    },
+})
+app.mount('#app')
